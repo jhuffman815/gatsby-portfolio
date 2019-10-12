2d144ad4bedbb2f77d64a67a48b4f4e8d036dc80
Jewell <jhuffman815@gmail.com>
2019-10-12 12:29:31 -0400

    Added projects

--- src/pages/works.js
+++ src/pages/works.js
@@ -4,14 +4,12 @@ import Layout from "../components/layout";
 import Project from "../components/Project";
 
 const projects = [
-    {image: "printbill.png", title: "PrintBill", link: "https://www.printbill.in/"},
-    {image: "sprung.png", title: "Sprung", link: "https://sprung.us"},
-    {image: "orange.png", title: "Orange Health", link: "https://whispering-bastion-31600.herokuapp.com/"},
-    {image: "billing.png", title: "Billing Restro", link: "https://billingrestro-react-prod.herokuapp.com/"},
-    {image: "ferrarisports.png", title: "Ferrari Sports", link: "http://ferrarisports.com/"},
-    {image: "pregnancy.png", title: "Pregnancy info", link: "http://pregnancy.info/"},
-    {image: "jaagastudy.png", title: "Jaaga Study", link: "https://nabendu82.github.io/incognosco/index.html"},
-    {image: "responsive1.png", title: "Responsive Site- POC", link: "https://shikhacorps.in/corps/"},
+    {image: "newtube.png", title: "Newtube", link: " https://jhuffman-youtube-clone.netlify.com/"},
+    {image: "evernote.png", title: "Evernote-Clone", link: "https://jhuffman-evernote-clone.netlify.com"},
+    {image: "tictactoe.png", title: "Tic-Tac-Toe", link: "https://jhuffman815.github.io/tic-tac-toe/"},
+    {image: "calculator.png", title: "Calculator", link: " https://jhuffman815.github.io/calculator/"},
+    {image: "cryptocurrency.png", title: "Cryptocurrency App", link: "https://github.com/jhuffman815/react-native-redux-crypto-tracker/"},
+    {image: "quote.png", title: "Quote Generator", link: "https://jhuffman815.github.io/Quote-Generator/"},
     {image: "responsive2.png", title: "Responsive Site2- POC", link: "https://shikhacorps.in/cssgridresponsive/"},
     {image: "styleconferences.png", title: "Style Conferences", link: "https://nabendu82.github.io/shayhowe/index.html"},
     {image: "itunes.png", title: "iTunes Clone - POC", link: "https://shikhacorps.in/mytunes/"},
@@ -25,7 +23,7 @@ const projects = [
 export default () => (
     <Layout>
         <Works>
-            <GenericH2 none dark some style={{textAlign: 'center'}}>Our Works</GenericH2>
+            <GenericH2 none dark some style={{textAlign: 'center'}}>Projects</GenericH2>
             <section class="gallery__flex">
              { projects && projects.map(proj => <Project key={proj.title} project={proj} />)}
             </section>
